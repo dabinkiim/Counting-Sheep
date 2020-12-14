9742e93f01d42a2006a9210efe51b4c758c1045e
dabinkiim <siskdb0103@gmail.com>
2020-12-15 00:16:53 +0900

    first update

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  var $output = $("#output");
+  var count = 0;
+  var timerID=0;
+  var sheep_count = "";
+
+  timerID = setInterval(function() {
+    //값 증가
+    count++;
+
+    //양 출력
+    sheep_count = "";
+    for (i = 0; i <count; i++) {
+      sheep_count += "🐑";
+    }
+    $('.count').html(sheep_count);
+
+    //제목으로 출력
+    document.title = count;
+
+    //값 출력
+    $output.text("");
+    $output.text(count);
+  },1000);
+
+})
